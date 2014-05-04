8476c6fea433fa31067406412e3aff7199439b63
zhangtao07 <zhang.tao.1006@163.com>
2014-05-04 13:31:42 +0800

    添加生成多维map.json文件及md5的支持

--- index.js
+++ index.js
@@ -42,7 +42,27 @@ module.exports = function(ret, conf, settings, opt) {
         info['hash'] = fis.util.md5(id, 7);
     });
 
-
+    //处理自动打包插件传递的多维的打包数据(如国际化多国家map配置),生成md5及多维的map.json
+    var retList = opt['retlist']; 
+    if(retList){
+        //给多维度的资源添加md5
+        _.map(retList,function(dimen,_ret){
+            if(dimen!="default"){ //默认维度不处理
+                _.map(_ret.res, function(id, info) {
+                    info['hash'] = fis.util.md5(id, 7);
+                });  
+                //生成多个维度的map.json
+                var map = fis.file(root, (ns ? ns + '-' : '') + 'map-' + dimen +'.json');
+                //修正map文件放置目录，config下
+                map.release = "/config" + map.subpath;
+                map.setContent(JSON.stringify(_ret, null, opt.optimize ? null : 4));      
+                ret.pkg[map.subpath] = map;  
+            }            
+        });
+    }
+    
+    
+    
     //create map.json
     var map = fis.file(root, (ns ? ns + '-' : '') + 'map.json');
     map.useHash = false;
